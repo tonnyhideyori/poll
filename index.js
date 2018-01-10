bf1f77d8a210fc5e7a1731d0d0ce9683738e648c
edwin ismail <typhoon_ed@yahoo.com>
2018-01-10 18:00:34 +0300

    auth backend deployment

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express=require("express");
+const express = require("express");
+const passport=require("passport")
+const cookieSession=require("cookie-session")
+const keys = require("./server/config/keys");
+const mongoose=require("mongoose");
+mongoose.connect(keys.mongoURI);
+require("./models/User");
+require("./services/passport")
+const app = express();
 
-const app=express();
+app.use(
+    cookieSession({
+        maxAge:12*30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+)
 
-app.get("/",(req,res)=>{
-res.send({hi :"there"});
-});
+app.use(passport.initialize());
+app.use(passport.session());
+require("./routes/authRoutes")(app);
 
-
-const PORT=process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
